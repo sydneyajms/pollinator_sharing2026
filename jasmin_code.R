142f209e144cba7a0332a8be4de710dc3842d212
jasminalbert <pimsupaa@uoregon.edu>
2026-01-28 13:45:14 -0800

    made heatmap image of interaction matrix

--- jasmin_code.R
+++ jasmin_code.R
@@ -35,6 +35,31 @@ for (tt in trts){
   clist[[tt]] <- ppmat
 }
 
+H_values <- lapply(clist, networklevel, index="H2")
+overlap <- lapply(clist, grouplevel, index='niche overlap')
+lapply(clist, plotweb)
+par(mfrow=c(2,3))
+lapply(clist, visweb, text='interaction', labsize=.8)
+
+cols <- hcl.colors(51, "PuBuGn", rev=T)
+zmax <- max(unlist(clist))
+par(mfrow=c(2,3), mar=c(1,1,1,1), bty='n')
+for (i in trts){
+  Opolls <- order(colSums(clist[[i]]), decreasing=T)
+  Oplants <- order(rowSums(clist[[i]]), decreasing=F)
+  tmp <- clist[[i]][Oplants, Opolls]
+  image(t(tmp), zlim=c(0, zmax), col=cols, asp=1)
+}
+
+
+
+library(RColorBrewer)
+cols <- hcl.colors(10, "YlOrRd")
+cols <- brewer.pal(9, "YlOrRd")
+par(mfrow =c(2,3))
+lapply(clist, visweb, box.col = "black", def.col = cols, 
+ labsize = 0.8, text = "interaction", textcol = "white")
+
 # join bee data to plant data 
 network <- pollinators %>%
   inner_join(seeds, by = "Stand", relationship = "many-to-many") %>%
@@ -68,4 +93,15 @@ for(i in seq_along(treatment_df)){
 
 #whole experiment network:
 pp_all <- as.matrix(table(pollinators$plant, pollinators$Genus))
+
+
+
+library(bipartite)
+
+
+head(seeds)
+
+
+
+
 
